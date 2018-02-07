ab8bc4fd28653114ba30c009005aa886f6584fc2
Thyholt <a.thyholt.henriksen@outlook.com>
2018-02-07 13:38:00 +0100

    main.c gjort kjørbar.

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("Hello world!\n";
+	printf("Hello world!\n");
 	printf("... and Mars \n");
 	if (1 > 0) {
 		return 1;
 	}
-	
->>>>>>> other
 	return 0;
 }
